1f4aa8f5d1c74c3cf7ba6194ebf7b49c5f228583
saintedlama <christoph.walcher@gmail.com>
2015-12-07 17:49:31 +0100

    Add files before commit

--- build.js
+++ build.js
@@ -57,6 +57,7 @@ metalsmith(__dirname)
       exec('git checkout gh-pages');
       cp('-rf', 'build/*', '.');
 
+      exec(`git add *`);
       exec(`git commit -a -m "${process.argv[3]}"`);
       exec(`git push`);
     }
